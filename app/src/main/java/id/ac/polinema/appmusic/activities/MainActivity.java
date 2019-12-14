126daf261b2059ea26ef86f69a5a8462eb245edb
Angga Maulana Athaariq <am.athaariq@gmail.com>
2019-12-15 02:53:31 +0700

    Add Shared Preferences

--- app/src/main/java/id/ac/polinema/appmusic/activities/MainActivity.java
+++ app/src/main/java/id/ac/polinema/appmusic/activities/MainActivity.java
@@ -1,11 +1,11 @@
 package id.ac.polinema.appmusic.activities;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-
-import android.os.Bundle;
-import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
