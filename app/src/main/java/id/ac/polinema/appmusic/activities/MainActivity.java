59a8e991f8c3a39d32b2a3f815b368620eea610a
Angga Maulana Athaariq <am.athaariq@gmail.com>
2019-12-15 01:27:22 +0700

    Add Splash Activity

--- app/src/main/java/id/ac/polinema/appmusic/activities/MainActivity.java
+++ app/src/main/java/id/ac/polinema/appmusic/activities/MainActivity.java
@@ -1,16 +1,63 @@
 package id.ac.polinema.appmusic.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import id.ac.polinema.appmusic.R;
+import id.ac.polinema.appmusic.fragments.AlbumFragment;
+import id.ac.polinema.appmusic.fragments.BioFragment;
+import id.ac.polinema.appmusic.fragments.HomeFragment;
+import id.ac.polinema.appmusic.fragments.MusicFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadFragment(new HomeFragment());
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+        // beri listener pada saat item/menu bottomnavigation terpilih
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .addToBackStack(null)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()) {
+            case R.id.action_home:
+                fragment = new HomeFragment();
+                break;
+            case R.id.action_bio:
+                fragment = new BioFragment();
+                break;
+            case R.id.action_music:
+                fragment = new MusicFragment();
+                break;
+            case R.id.action_album:
+                fragment = new AlbumFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
